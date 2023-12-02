63ed83a2a976c381c0258cfab1694c352313ccbb
Caen De Silva <caen@desilva.se>
2023-12-02 17:21:26 +0100

    Replace qualifier with an import

--- tests/Unit/BootstrapTest.php
+++ tests/Unit/BootstrapTest.php
@@ -1,7 +1,9 @@
 <?php
 
+use Hyde\Foundation\Application;
+
 $app = require __DIR__ . '/../../app/bootstrap.php';
 
 test('bootstrapper returns application', function () use ($app) {
-    expect($app)->toBeInstanceOf(\Hyde\Foundation\Application::class);
+    expect($app)->toBeInstanceOf(Application::class);
 });
