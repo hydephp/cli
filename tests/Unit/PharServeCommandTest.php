4ddb112f66a95098448b9f877fabe622c794178a
Caen De Silva <caen@desilva.se>
2023-12-02 19:51:31 +0100

    Add testable command class

--- tests/Unit/PharServeCommandTest.php
+++ tests/Unit/PharServeCommandTest.php
@@ -1,1 +1,29 @@
-<?php
+<?php /** @noinspection PhpIllegalPsrClassPathInspection */
+
+use App\Commands\PharServeCommand;
+use Symfony\Component\Console\Input\ArrayInput;
+use Symfony\Component\Console\Output\BufferedOutput;
+
+class TestablePharServeCommand extends PharServeCommand
+{
+    public function __construct()
+    {
+        parent::__construct();
+
+        $this->input = tap(Mockery::mock(ArrayInput::class), function ($mock) {
+            $mock->shouldReceive('getOption')->with('no-ansi')->andReturn('false');
+            $mock->shouldReceive('getOption')->with('host')->andReturn('localhost');
+            $mock->shouldReceive('getOption')->with('port')->andReturn(8080);
+            $mock->shouldReceive('getOption')->with('save-preview')->andReturnNull();
+            $mock->shouldReceive('getOption')->with('dashboard')->andReturnNull();
+            $mock->shouldReceive('getOption')->with('pretty-urls')->andReturnNull();
+            $mock->shouldReceive('getOption')->with('play-cdn')->andReturnNull();
+        });
+        $this->output = new BufferedOutput();
+    }
+
+    public function getEnvironmentVariables(): array
+    {
+        return parent::getEnvironmentVariables();
+    }
+}
