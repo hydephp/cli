56a8222873fdf0fb0261b7ab418e7524867d365b
Caen De Silva <caen@desilva.se>
2023-12-03 18:25:14 +0100

    Create NewProjectCommand.php

---
+++ app/Commands/NewProjectCommand.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Commands;
+
+use Illuminate\Console\Command;
+
+/**
+ * Creates a new Hyde project.
+ */
+class NewProjectCommand extends Command
+{
+    //
+}
