2011f8a7447c6de2274d3553838cee491ea5201d
Caen De Silva <caen@desilva.se>
2023-12-03 20:21:08 +0100

    Create NewProjectCommandTest.php

---
+++ tests/Unit/NewProjectCommandTest.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Foundation\Application;
+use Illuminate\Support\Facades\Process;
+use Illuminate\Contracts\Console\Kernel;
+
+function bootstrap(): Application
+{
+    $app = require __DIR__ . '/../../app/bootstrap.php';
+
+    $app->make(Kernel::class)->bootstrap();
+
+    return $app;
+}
+
+test('can create new project', function () {
+    $app = bootstrap();
+
+    Process::preventStrayProcesses();
+
+    Process::shouldReceive('command')->once()->with('composer create-project hyde/hyde test-project --prefer-dist --ansi')->andReturnSelf();
+
+    Process::shouldReceive('run')->once()->withArgs([null, Mockery::type(Closure::class)])->andReturnSelf();
+
+    $app->make(Kernel::class)->call('new', ['name' => 'test-project']);
+});
