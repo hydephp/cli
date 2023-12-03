909046b5be9f6617d12a3d45f9c873b499ee0a17
Caen De Silva <caen@desilva.se>
2023-12-03 11:57:35 +0100

    Create Assert.php

---
+++ tests/System/bin/Assert.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * @internal A micro CLI tool to run cross-platform high-level assertions from the command line
+ *
+ * @example php Assert.php '10 >= 5'
+ * @example php Assert.php 'file_exists("composer.json")'
+ * @example php Assert.php 'file_exists("composer.json")' 'is_dir("vendor")'
+ */
+
+$assertions = array_slice($argv, 1);
+
+if (empty($assertions)) {
+    echo 'No assertions provided' . PHP_EOL;
+    exit(1);
+}
+
+foreach ($assertions as $assertion) {
+    if (eval("return $assertion;")) {
+        echo "Assertion passed: $assertion" . PHP_EOL;
+    } else {
+        echo "Assertion failed: $assertion" . PHP_EOL;
+        exit(1);
+    }
+}
+
+exit(0);
