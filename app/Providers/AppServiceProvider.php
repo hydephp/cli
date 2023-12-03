d4e3df6e0131ef065a779ba47ba5f8530d624518
Caen De Silva <caen@desilva.se>
2023-12-03 22:18:15 +0100

    Register the new project command

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -11,7 +11,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        $this->commands([
+            \App\Commands\NewProjectCommand::class,
+        ]);
     }
 
     /**
