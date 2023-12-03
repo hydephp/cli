4d92494c56c406b5a9aa27c01ca9ce8b2ba3d5ad
Caen De Silva <caen@desilva.se>
2023-12-03 18:33:51 +0100

    Create base handle method

--- app/Commands/NewProjectCommand.php
+++ app/Commands/NewProjectCommand.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace App\Commands;
 
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Process;
 
 /**
  * Creates a new Hyde project.
@@ -16,4 +17,14 @@ class NewProjectCommand extends Command
 
     /** @var string */
     protected $description = 'Create a new Hyde project.';
+
+    public function handle(): void
+    {
+        $name = $this->argument('name');
+
+        $this->info("Creating new Hyde project: {$name}");
+
+        Process::command("composer create-project hyde/hyde {$name} --prefer-dist")
+            ->run();
+    }
 }
