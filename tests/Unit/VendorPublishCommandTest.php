a39f91a6c7ff03203aaf1281a931566ca9525f07
Caen De Silva <caen@desilva.se>
2023-12-07 20:46:07 +0100

    Create VendorPublishCommandTest.php

---
+++ tests/Unit/VendorPublishCommandTest.php
@@ -0,0 +1,19 @@
+<?php /** @noinspection PhpIllegalPsrClassPathInspection */
+
+use Illuminate\Filesystem\Filesystem;
+use App\Commands\VendorPublishCommand;
+
+it('rewrites vendor paths to lead to phar path', function () {
+    $command = new VendorPublishCommandTestClass(new Filesystem());
+    $path = 'vendor/hyde/framework/src/Commands/VendorPublishCommand.php';
+
+    expect(($command)->normalizePath($path))->not()->toBe($path);
+});
+
+class VendorPublishCommandTestClass extends VendorPublishCommand
+{
+    public function normalizePath(string $from): string
+    {
+        return parent::normalizePath($from);
+    }
+}
