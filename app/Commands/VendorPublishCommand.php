97cf69ab00a707f84276c4c2d51be6528fcf4f14
Caen De Silva <caen@desilva.se>
2023-12-07 20:30:32 +0100

    Support vendor publishing from standalone

--- app/Commands/VendorPublishCommand.php
+++ app/Commands/VendorPublishCommand.php
@@ -4,7 +4,19 @@ declare(strict_types=1);
 
 namespace App\Commands;
 
+use Illuminate\Support\Str;
+use Hyde\Foundation\PharSupport;
+use function Hyde\normalize_slashes;
+
+/** @experimental */
 class VendorPublishCommand extends \Illuminate\Foundation\Console\VendorPublishCommand
 {
-    //
+    /** @internal Provides Phar support */
+    protected function publishItem($from, $to)
+    {
+        $from = Str::after(normalize_slashes($from), 'vendor/hyde/framework/');
+        $from = PharSupport::vendorPath($from);
+
+        parent::publishItem($from, $to);
+    }
 }
