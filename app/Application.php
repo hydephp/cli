83466bfdb199a3604df40425a326c2a2cfd4e350
Caen De Silva <caen@desilva.se>
2023-12-08 11:29:07 +0100

    Refactor bootstrapper to use custom application class

--- app/Application.php
+++ app/Application.php
@@ -4,7 +4,21 @@ declare(strict_types=1);
 
 namespace App;
 
-class Application
+class Application extends \Hyde\Foundation\Application
 {
-    //
+    public function getCachedPackagesPath(): string
+    {
+        // Since we have a custom path for the cache directory, we need to return it here.
+        return HYDE_TEMP_DIR . '/app/storage/framework/cache/packages.php';
+    }
+
+    public function getNamespace()
+    {
+        if (file_exists($this->basePath('composer.json'))) {
+            return parent::getNamespace();
+        }
+
+        // Adds a fallback so that the application can still run without a composer.json file
+        return 'App';
+    }
 }
--- app/bootstrap.php
+++ app/bootstrap.php
@@ -21,23 +21,7 @@ if (! defined('HYDE_TEMP_DIR')) {
 |
 */
 
-$app = new class(HYDE_WORKING_DIR) extends \Hyde\Foundation\Application {
-    public function getCachedPackagesPath(): string
-    {
-        // Since we have a custom path for the cache directory, we need to return it here.
-        return HYDE_TEMP_DIR . '/app/storage/framework/cache/packages.php';
-    }
-
-    public function getNamespace()
-    {
-        if (file_exists($this->basePath('composer.json'))) {
-            return parent::getNamespace();
-        }
-
-        // Adds a fallback so that the application can still run without a composer.json file
-        return 'App';
-    }
-};
+$app = new \App\Application(HYDE_WORKING_DIR);
 
 /*
 |--------------------------------------------------------------------------
