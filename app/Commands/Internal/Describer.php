cab182762f191967bc173230fc053e38ce27e191
Caen De Silva <caen@desilva.se>
2023-12-09 22:01:26 +0100

    Override command sorting to move up `new` command in list
    Requires https://github.com/nunomaduro/laravel-console-summary/pull/20

--- app/Commands/Internal/Describer.php
+++ app/Commands/Internal/Describer.php
@@ -8,5 +8,14 @@ use NunoMaduro\LaravelConsoleSummary\Describer as BaseDescriber;
 
 class Describer extends BaseDescriber
 {
-    //
+    protected static function sortCommandsInGroup(array &$commands): void
+    {
+        usort($commands, function ($a, $b) {
+            if  ($a->getName() === 'new') {
+                return -1;
+            }
+
+            return strcmp($a->getName(), $b->getName());
+        });
+    }
 }
