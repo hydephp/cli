c1b31d901ae78cbbe4cdaa13382d6fb4a80b659c
Caen De Silva <caen@desilva.se>
2023-12-09 22:00:19 +0100

    Create custom console describer implementation

---
+++ app/Commands/Internal/Describer.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Commands\Internal;
+
+use NunoMaduro\LaravelConsoleSummary\Describer as BaseDescriber;
+
+class Describer extends BaseDescriber
+{
+    //
+}
