0ab2ec15df5c03769e036405caac85fe4039ebf9
Caen De Silva <caen@desilva.se>
2023-12-10 16:11:45 +0100

    Wip

--- bin/get-release-version.php
+++ bin/get-release-version.php
@@ -2,4 +2,8 @@
 
 require_once __DIR__.'/../vendor/autoload.php';
 
-echo 'v'.\App\Application::APP_VERSION;
+$application = file_get_contents(__DIR__.'/../app/Application.php');
+
+echo trim(str_replace(['final public const APP_VERSION', '=', '\'', ';'], '', array_values(
+    array_filter(explode("\n", $application), fn ($line) => str_contains($line, 'APP_VERSION'))
+)[0]));
