4863c08cd7786aab2e6b8a9ce8b7909fcda26ff8
Caen De Silva <caen@desilva.se>
2023-12-12 10:04:01 +0100

    Refactor and formatting

--- SyncTraffic.php
+++ SyncTraffic.php
@@ -7,7 +7,6 @@
  *
  * @see https://docs.github.com/en/rest/metrics/traffic?apiVersion=2022-11-28
  */
-
 echo "Syncing traffic data!\n";
 
 // Check if --debug is passed as an argument, if so, enable debug mode.
@@ -23,127 +22,160 @@ assert(str_starts_with($accessToken, 'github_pat_'), 'Invalid access token');
 
 $database = json_decode(file_get_contents('database.json'), true);
 
-function getResponse(string $endpoint): array
+class SyncTraffic
 {
-    $name = match ($endpoint) {
-        'clones' => 'clones',
-        'views' => 'views',
-        'popular/paths' => 'popular paths',
-        'popular/referrers' => 'popular referrers',
-        default => null,
-    };
-    assert($name !== null, 'Invalid endpoint');
-    echo " - Fetching ". $name ."... ";
+    private array $database;
 
-    //curl - L \
-    //  -H 'Accept: application/vnd.github+json' \
-    //  -H 'Authorization: Bearer <YOUR-TOKEN>' \
-    //  -H 'X-GitHub-Api-Version: 2022-11-28' \
-    //https://api.github.com/repos/OWNER/REPO/traffic/clones
+    private string $repo;
 
-    global $debug;
-    global $repo;
-    global $accessToken;
+    private string $accessToken;
 
-    $url = "https://api.github.com/repos/{$repo}/traffic/".$endpoint;
+    private bool $debug;
 
-    $userAgent = 'HydePHP Traffic Controller';
-
-    $ch = curl_init($url);
-
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-    curl_setopt($ch, CURLOPT_HTTPHEADER, [
-        'Accept: application/vnd.github+json',
-        "Authorization: Bearer {$accessToken}",
-        'X-GitHub-Api-Version: 2022-11-28',
-        "User-Agent: {$userAgent}",
-    ]);
-
-    $response = curl_exec($ch);
-
-    if (curl_errno($ch)) {
-        throw new \Exception('Curl error: '.curl_error($ch));
+    public function __construct(array $database, string $repo, string $accessToken, bool $debug = false)
+    {
+        $this->database = $database;
+        $this->repo = $repo;
+        $this->accessToken = $accessToken;
+        $this->debug = $debug;
     }
 
-    // Check status code
-    $statusCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-    if ($statusCode >= 400) {
-        throw new \Exception('Invalid status code: '.$statusCode."\n".$response);
+    public function fetch(): array
+    {
+        return $this->fetchTraffic();
     }
 
-    curl_close($ch);
+    private function getResponse(string $endpoint): array
+    {
+        $name = match ($endpoint) {
+            'clones' => 'clones',
+            'views' => 'views',
+            'popular/paths' => 'popular paths',
+            'popular/referrers' => 'popular referrers',
+            default => null,
+        };
+        assert($name !== null, 'Invalid endpoint');
+        echo ' - Fetching '.$name.'... ';
 
-    // $response now contains the API response
-    $data = json_decode($response, true);
+        //curl - L \
+        //  -H 'Accept: application/vnd.github+json' \
+        //  -H 'Authorization: Bearer <YOUR-TOKEN>' \
+        //  -H 'X-GitHub-Api-Version: 2022-11-28' \
+        //https://api.github.com/repos/OWNER/REPO/traffic/clones
 
-    if (! is_array($data)) {
-        throw new \Exception('Invalid response: '."\n".$response);
+        $debug = $this->debug;
+        $repo = $this->repo;
+        $accessToken = $this->accessToken;
+
+        $url = "https://api.github.com/repos/{$repo}/traffic/".$endpoint;
+
+        $userAgent = 'HydePHP Traffic Controller';
+
+        $ch = curl_init($url);
+
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, [
+            'Accept: application/vnd.github+json',
+            "Authorization: Bearer {$accessToken}",
+            'X-GitHub-Api-Version: 2022-11-28',
+            "User-Agent: {$userAgent}",
+        ]);
+
+        $response = curl_exec($ch);
+
+        if (curl_errno($ch)) {
+            throw new \Exception('Curl error: '.curl_error($ch));
+        }
+
+        // Check status code
+        $statusCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+        if ($statusCode >= 400) {
+            throw new \Exception('Invalid status code: '.$statusCode."\n".$response);
+        }
+
+        curl_close($ch);
+
+        // $response now contains the API response
+        $data = json_decode($response, true);
+
+        if (! is_array($data)) {
+            throw new \Exception('Invalid response: '."\n".$response);
+        }
+
+        $padding = 20 - strlen($name);
+
+        echo str_pad(' ', $padding)."Done!\n";
+
+        if ($debug) {
+            echo '<response>';
+            print_r($data);
+            echo '</response>';
+        }
+
+        return $data;
     }
 
-    $padding =  20-strlen($name);
+    private function fetchTraffic(): array
+    {
+        $database = $this->database;
+        $clones = $this->getResponse('clones');
 
-    echo str_pad(' ', $padding) ."Done!\n";
+        foreach ($clones['clones'] as $clone) {
+            $database['traffic'][$clone['timestamp']]['clones'] = [
+                'count' => $clone['count'],
+                'uniques' => $clone['uniques'],
+            ];
+        }
 
-    if ($debug) {
-        echo '<response>';
-        print_r($data);
-        echo '</response>';
+        $views = $this->getResponse('views');
+
+        foreach ($views['views'] as $view) {
+            $database['traffic'][$view['timestamp']]['views'] = [
+                'count' => $view['count'],
+                'uniques' => $view['uniques'],
+            ];
+        }
+
+        // We store these under each year-month, so we can have some sort of tracking over time.
+        // The reason we do this monthly is because we can't get the data for a specific date, only the last 14 days,
+        // and since we can't know which day some data is for, we don't know when it overlaps with the previous data if
+        // we fetch data for the same day multiple times. Having it stored monthly means we get an average that is at least somewhat accurate.
+        $popularDataKey = date('Y-m', (time()));
+
+        $popularPaths = $this->getResponse('popular/paths');
+
+        foreach ($popularPaths as $path) {
+            // Since the paths are messy, we use a hash of the path as the key.
+            $key = hash('sha256', $path['path']);
+
+            $existing = $database['popular'][$popularDataKey]['paths'][$key] ?? [];
+            $database['popular'][$popularDataKey]['paths'][$key] = [
+                'path' => $path['path'],
+                'title' => $path['title'],
+                'count' => max($path['count'], $existing['count'] ?? 0),
+                'uniques' => max($path['uniques'], $existing['uniques'] ?? 0),
+            ];
+        }
+
+        $popularReferrers = $this->getResponse('popular/referrers');
+
+        foreach ($popularReferrers as $referrer) {
+            $existing = $database['popular'][$popularDataKey]['referrers'][$referrer['referrer']] ?? [];
+            $database['popular'][$popularDataKey]['referrers'][$referrer['referrer']] = [
+                'count' => max($referrer['count'], $existing['count'] ?? 0),
+                'uniques' => max($referrer['uniques'], $existing['uniques'] ?? 0),
+            ];
+        }
+
+        // Save the database
+        echo 'Saving database... ';
+
+        return $database;
     }
-
-    return $data;
 }
 
-$clones = getResponse('clones');
-
-foreach ($clones['clones'] as $clone) {
-    $database['traffic'][$clone['timestamp']]['clones'] = [
-        'count' => $clone['count'],
-        'uniques' => $clone['uniques'],
-    ];
-}
-
-$views = getResponse('views');
-
-foreach ($views['views'] as $view) {
-    $database['traffic'][$view['timestamp']]['views'] = [
-        'count' => $view['count'],
-        'uniques' => $view['uniques'],
-    ];
-}
-
-// We store these under each year-month, so we can have some sort of tracking over time.
-// The reason we do this monthly is because we can't get the data for a specific date, only the last 14 days,
-// and since we can't know which day some data is for, we don't know when it overlaps with the previous data if
-// we fetch data for the same day multiple times. Having it stored monthly means we get an average that is at least somewhat accurate.
-$popularDataKey = date('Y-m', (time()));
-
-$popularPaths = getResponse('popular/paths');
-
-foreach ($popularPaths as $path) {
-    // Since the paths are messy, we use a hash of the path as the key.
-    $key = hash('sha256', $path['path']);
-
-    $existing = $database['popular'][$popularDataKey]['paths'][$key] ?? [];
-    $database['popular'][$popularDataKey]['paths'][$key] = [
-        'path' => $path['path'],
-        'title' => $path['title'],
-        'count' => max($path['count'], $existing['count'] ?? 0),
-        'uniques' => max($path['uniques'], $existing['uniques'] ?? 0),
-    ];
-}
-
-$popularReferrers = getResponse('popular/referrers');
-
-foreach ($popularReferrers as $referrer) {
-    $existing = $database['popular'][$popularDataKey]['referrers'][$referrer['referrer']] ?? [];
-    $database['popular'][$popularDataKey]['referrers'][$referrer['referrer']] = [
-        'count' => max($referrer['count'], $existing['count'] ?? 0),
-        'uniques' => max($referrer['uniques'], $existing['uniques'] ?? 0),
-    ];
-}
-
-// Save the database
-echo "Saving database... ";
+$syncTraffic = new SyncTraffic($database, $repo, $accessToken, $debug);
+$database = $syncTraffic->fetch();
 
 file_put_contents('database.json', json_encode($database, JSON_PRETTY_PRINT));
 
