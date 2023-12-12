26795e1f9059b73082324d93557887f6dbee02a3
Caen De Silva <caen@desilva.se>
2023-12-12 11:48:28 +0100

    Validate the data integrity

--- sync.php
+++ sync.php
@@ -22,6 +22,8 @@ echo 'Saving database... ';
 
 $database = updateDatabaseMetadata($database);
 
+validateDatabaseSchema($database);
+
 file_put_contents('database.json', json_encode($database, JSON_PRETTY_PRINT));
 
 echo "Done!\n";
@@ -54,4 +56,73 @@ function updateDatabaseMetadata(array $database): array
     $database['_database']['content_hash'] = $contentHash;
 
     return $database;
+}
+
+/** Validate the data integrity */
+function validateDatabaseSchema(array $database): void
+{
+    foreach ($database as $tableKey => $table) {
+        assert(in_array($tableKey, ['_database', 'traffic', 'popular']));
+        assert(is_array($table));
+
+        switch ($tableKey) {
+            case '_database':
+                assert(array_key_exists('last_updated', $table));
+                assert(array_key_exists('content_hash', $table));
+
+                assert(is_int($table['last_updated']));
+                assert(is_string($table['content_hash']));
+                assert(strlen($table['content_hash']) === 64);
+                break;
+            case 'traffic':
+                foreach ($table as $dateKey => $date) {
+                    assert(is_string($dateKey));
+                    assert(strlen($dateKey) === 20);
+                    assert(str_ends_with($dateKey, 'T00:00:00Z'));
+
+                    assert(array_key_exists('views', $date));
+                    assert(array_key_exists('clones', $date));
+
+                    assert(is_array($date['views']));
+                    assert(is_array($date['clones']));
+
+                    assert(array_key_exists('count', $date['views']));
+                    assert(array_key_exists('uniques', $date['views']));
+                    assert(array_key_exists('count', $date['clones']));
+                    assert(array_key_exists('uniques', $date['clones']));
+
+                    assert(is_int($date['views']['count']));
+                    assert(is_int($date['views']['uniques']));
+                    assert(is_int($date['clones']['count']));
+                    assert(is_int($date['clones']['uniques']));
+                }
+                break;
+            case 'popular':
+                foreach ($table as $dateKey => $date) {
+                    assert(is_string($dateKey));
+                    assert(strlen($dateKey) === 7);
+                    assert(str_contains($dateKey, '-'));
+
+                    assert(array_key_exists('paths', $date));
+
+                    assert(is_array($date['paths']));
+
+                    foreach ($date['paths'] as $pathKey => $path) {
+                        assert(is_string($pathKey));
+                        assert(strlen($pathKey) === 64);
+
+                        assert(array_key_exists('path', $path));
+                        assert(array_key_exists('title', $path));
+                        assert(array_key_exists('count', $path));
+                        assert(array_key_exists('uniques', $path));
+
+                        assert(is_string($path['path']));
+                        assert(is_string($path['title']));
+                        assert(is_int($path['count']));
+                        assert(is_int($path['uniques']));
+                    }
+                }
+                break;
+        }
+    }
 }
