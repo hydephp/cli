fbb8c70763cc5e9b6fef4e8c0edd297315efd5bb
Caen De Silva <caen@desilva.se>
2023-12-13 11:02:46 +0100

    Revert "Proof of concept"
    This reverts commit 5301e9fe8017c0e0c8e87448e98eeddcdb7fbb0b.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -7,12 +7,7 @@ use App\Commands\NewProjectCommand;
 use App\Commands\ServeCommand;
 use App\Commands\VendorPublishCommand;
 use Illuminate\Support\ServiceProvider;
-use Humbug\SelfUpdate\Updater as PharUpdater;
-use LaravelZero\Framework\Providers\Build\Build;
-use LaravelZero\Framework\Components\Updater\Updater;
 use NunoMaduro\LaravelConsoleSummary\Contracts\DescriberContract;
-use LaravelZero\Framework\Components\Updater\Strategy\GithubStrategy;
-use LaravelZero\Framework\Components\Updater\Strategy\StrategyInterface;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -42,30 +37,5 @@ class AppServiceProvider extends ServiceProvider
 
         // Register custom Laravel summary command describer implementation.
         $this->app->singleton(DescriberContract::class, Describer::class);
-
-        $build = $this->app->make(Build::class);
-
-        if ($build->isRunning() && $this->app->environment('production')) {
-            $this->app->singleton(Updater::class, function () use ($build) {
-                $updater = new PharUpdater($build->getPath(), false, PharUpdater::STRATEGY_GITHUB);
-
-                $composer = json_decode(file_get_contents(__DIR__.'/../../composer.json'), true);
-                $name = $composer['name'];
-
-                $strategy = $this->app['config']->get('updater.strategy', GithubStrategy::class);
-
-                $updater->setStrategyObject($this->app->make($strategy));
-
-                if ($updater->getStrategy() instanceof StrategyInterface) {
-                    $updater->getStrategy()->setPackageName($name);
-                }
-
-                if (method_exists($updater->getStrategy(), 'setCurrentLocalVersion')) {
-                    $updater->getStrategy()->setCurrentLocalVersion(config('app.version'));
-                }
-
-                return new Updater($updater);
-            });
-        }
     }
 }
