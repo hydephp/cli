a55c431f57890ccb3a78d0f06c23b9fb24a9daf5
Caen De Silva <caen@desilva.se>
2024-04-16 21:26:58 +0200

    Sort import

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,9 +2,9 @@
 
 namespace App\Providers;
 
-use App\Commands\SelfUpdateCommand;
 use App\Commands\Internal\Describer;
 use App\Commands\NewProjectCommand;
+use App\Commands\SelfUpdateCommand;
 use App\Commands\ServeCommand;
 use App\Commands\VendorPublishCommand;
 use Illuminate\Support\ServiceProvider;
