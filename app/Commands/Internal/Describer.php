ccd05b8a8635701231f63d4ac0af02b72ef6d6cb
Caen De Silva <caen@desilva.se>
2024-04-17 20:38:55 +0200

    Strongly type the closure values

--- app/Commands/Internal/Describer.php
+++ app/Commands/Internal/Describer.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace App\Commands\Internal;
 
+use Illuminate\Console\Command;
 use NunoMaduro\LaravelConsoleSummary\Describer as BaseDescriber;
 
 /**
@@ -15,7 +16,7 @@ class Describer extends BaseDescriber
 {
     protected static function sortCommandsInGroup(array &$commands): void
     {
-        usort($commands, function ($a, $b) {
+        usort($commands, function (Command $a, Command $b): int {
             if ($a->getName() === 'new') {
                 return -1;
             }
