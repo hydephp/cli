e8c23696020d6579de5ccbf175c4998ee2f3b388
Caen De Silva <caen@desilva.se>
2024-04-18 21:44:06 +0200

    Test it returns an array with integer values

--- tests/Unit/SelfUpdateCommandUnitTest.php
+++ tests/Unit/SelfUpdateCommandUnitTest.php
@@ -14,6 +14,21 @@ it('parses the version correctly', function ($input, $expectedOutput) {
     ['0.0.1', ['major' => 0, 'minor' => 0, 'patch' => 1]],
 ]);
 
+it('returns an array with integer values', function ($input, $expectedOutput) {
+    $class = new InspectableSelfUpdateCommand();
+
+    $result = $class->parseVersion($input);
+
+    expect($result)->toEqual($expectedOutput)
+        ->and($result['major'])->toBeInt()
+        ->and($result['minor'])->toBeInt()
+        ->and($result['patch'])->toBeInt();
+})->with([
+    ['1.2.3', ['major' => 1, 'minor' => 2, 'patch' => 3]],
+    ['2.5.0', ['major' => 2, 'minor' => 5, 'patch' => 0]],
+    ['0.0.1', ['major' => 0, 'minor' => 0, 'patch' => 1]],
+]);
+
 class InspectableSelfUpdateCommand extends SelfUpdateCommand
 {
     public function property(string $property): mixed
