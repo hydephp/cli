4dd748554d29bc62b3b31343a09b68aff670b412
Caen De Silva <caen@desilva.se>
2024-04-18 21:27:01 +0200

    Add inspectable self update command class

--- tests/Unit/SelfUpdateCommandUnitTest.php
+++ tests/Unit/SelfUpdateCommandUnitTest.php
@@ -1,2 +1,16 @@
 <?php
 
+use App\Commands\SelfUpdateCommand;
+
+class InspectableSelfUpdateCommand extends SelfUpdateCommand
+{
+    public function property(string $property): mixed
+    {
+        return $this->$property;
+    }
+
+    public function method(string $command, array $arguments = []): mixed
+    {
+        return $this->$command(...$arguments);
+    }
+}
