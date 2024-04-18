38a4f2317ea3f9e8fc0a4dffeec3b0e3dce55971
Caen De Silva <caen@desilva.se>
2024-04-18 21:38:48 +0200

    Use Pest dataset

--- tests/Unit/SelfUpdateCommandUnitTest.php
+++ tests/Unit/SelfUpdateCommandUnitTest.php
@@ -2,21 +2,17 @@
 
 use App\Commands\SelfUpdateCommand;
 
-it('parses the version correctly', function () {
+it('parses the version correctly', function ($input, $expectedOutput) {
     $class = new InspectableSelfUpdateCommand();
 
-    $testCases = [
-        ['1.2.3', ['major' => 1, 'minor' => 2, 'patch' => 3]],
-        ['2.5.0', ['major' => 2, 'minor' => 5, 'patch' => 0]],
-        ['0.0.1', ['major' => 0, 'minor' => 0, 'patch' => 1]],
-    ];
+    $result = $class->method('parseVersion', $input);
 
-    foreach ($testCases as [$input, $expectedOutput]) {
-        $result = $class->method('parseVersion', $input);
-
-        expect($result)->toBe($expectedOutput);
-    }
-});
+    expect($result)->toBe($expectedOutput);
+})->with([
+    ['1.2.3', ['major' => 1, 'minor' => 2, 'patch' => 3]],
+    ['2.5.0', ['major' => 2, 'minor' => 5, 'patch' => 0]],
+    ['0.0.1', ['major' => 0, 'minor' => 0, 'patch' => 1]],
+]);
 
 class InspectableSelfUpdateCommand extends SelfUpdateCommand
 {
