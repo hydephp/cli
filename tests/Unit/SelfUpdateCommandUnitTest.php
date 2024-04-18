1ce7ed74c207f1402843292dcf6a8ad9379dd301
Caen De Silva <caen@desilva.se>
2024-04-18 21:38:04 +0200

    Test it parses the version correctly

--- tests/Unit/SelfUpdateCommandUnitTest.php
+++ tests/Unit/SelfUpdateCommandUnitTest.php
@@ -2,6 +2,22 @@
 
 use App\Commands\SelfUpdateCommand;
 
+it('parses the version correctly', function () {
+    $class = new InspectableSelfUpdateCommand();
+
+    $testCases = [
+        ['1.2.3', ['major' => 1, 'minor' => 2, 'patch' => 3]],
+        ['2.5.0', ['major' => 2, 'minor' => 5, 'patch' => 0]],
+        ['0.0.1', ['major' => 0, 'minor' => 0, 'patch' => 1]],
+    ];
+
+    foreach ($testCases as [$input, $expectedOutput]) {
+        $result = $class->method('parseVersion', $input);
+
+        expect($result)->toBe($expectedOutput);
+    }
+});
+
 class InspectableSelfUpdateCommand extends SelfUpdateCommand
 {
     public function property(string $property): mixed
