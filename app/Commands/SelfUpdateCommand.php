6808e998df0ecc756ba396a6337340efa806b6e5
Caen De Silva <caen@desilva.se>
2024-04-19 19:37:44 +0200

    Revert "Add initial scaffolding for custom standard output stream"
    This reverts commit 855eba27b7ba36bad1cee56db12f3c6aecf0422e.

--- app/Commands/SelfUpdateCommand.php
+++ app/Commands/SelfUpdateCommand.php
@@ -390,12 +390,7 @@ class SelfUpdateCommand extends Command
         $command = self::COMPOSER_COMMAND;
 
         if (PHP_OS_FAMILY === 'Windows') {
-            // We need to run Composer as an administrator on Windows, so we use PowerShell to request a UAC prompt if needed
-            // Since this means that we lose the ability to capture the output, we redirect it to a temporary file instead
-
-            $stdout = tempnam(sys_get_temp_dir(), 'hyde');
-            touch($stdout);
-
+            // Attempt to run the command with the elevated privileges
             $powerShell = sprintf("Start-Process -Verb RunAs powershell -ArgumentList '-Command %s' -Wait", escapeshellarg($command));
             $command = 'powershell -Command "'.$powerShell.'"';
         }
@@ -404,12 +399,6 @@ class SelfUpdateCommand extends Command
             $this->output->writeln('<fg=gray> > '.trim($buffer).'</>');
             $output[] = $buffer;
         });
-
-        if (isset($stdout)) {
-            $output = file($stdout, FILE_IGNORE_NEW_LINES);
-            $this->output->writeln($output);
-            unlink($stdout);
-        }
 
         return [$result->exitCode(), $output];
     }
