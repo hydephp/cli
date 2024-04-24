332b09927afff251a7343c5140f197f720f055e3
Caen De Silva <caen@desilva.se>
2024-04-24 15:07:57 +0200

    Refactor test to be a Pest test

--- tests/Unit/Support/DescriberTest.php
+++ tests/Unit/Support/DescriberTest.php
@@ -1,40 +1,35 @@
 <?php
 
-use PHPUnit\Framework\TestCase;
 use Illuminate\Console\Command;
 use App\Commands\Internal\Describer;
 
-class DescriberTest extends TestCase
+test('test sort commands in group method', function () {
+    // Mock the Command class for testing
+    $command1 = createCommandMock('aaa');
+    $command2 = createCommandMock('new');
+    $command3 = createCommandMock('bbb');
+
+    // Call the method to be tested
+    $commands = [$command1, $command2, $command3];
+    TestDescriber::sortCommandsInGroup($commands);
+
+    // Assert that the commands are sorted correctly
+    $this->assertSame('new', $commands[0]->getName());
+    $this->assertSame('aaa', $commands[1]->getName());
+    $this->assertSame('bbb', $commands[2]->getName());
+
+    $this->assertSame(['new', 'aaa', 'bbb'], array_map(fn ($command) => $command->getName(), $commands));
+});
+
+function createCommandMock(string $name): Command
 {
-    public function testSortCommandsInGroup()
-    {
-        // Mock the Command class for testing
-        $command1 = $this->createCommandMock('aaa');
-        $command2 = $this->createCommandMock('new');
-        $command3 = $this->createCommandMock('bbb');
+    $command = test()->getMockBuilder(Command::class)
+        ->disableOriginalConstructor()
+        ->getMock();
 
-        // Call the method to be tested
-        $commands = [$command1, $command2, $command3];
-        TestDescriber::sortCommandsInGroup($commands);
+    $command->method('getName')->willReturn($name);
 
-        // Assert that the commands are sorted correctly
-        $this->assertSame('new', $commands[0]->getName());
-        $this->assertSame('aaa', $commands[1]->getName());
-        $this->assertSame('bbb', $commands[2]->getName());
-
-        $this->assertSame(['new', 'aaa', 'bbb'], array_map(fn ($command) => $command->getName(), $commands));
-    }
-
-    protected function createCommandMock(string $name): Command
-    {
-        $command = $this->getMockBuilder(Command::class)
-            ->disableOriginalConstructor()
-            ->getMock();
-
-        $command->method('getName')->willReturn($name);
-
-        return $command;
-    }
+    return $command;
 }
 
 class TestDescriber extends Describer
