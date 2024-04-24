7b9c9b952c657643184bd50f2ceb813416e68641
Caen De Silva <caen@desilva.se>
2024-04-24 12:14:11 +0200

    Extract better helper function

--- tests/Unit/Support/GitHubReleaseDataTest.php
+++ tests/Unit/Support/GitHubReleaseDataTest.php
@@ -1,26 +1,27 @@
 <?php
 
+use Illuminate\Support\Arr;
 use App\Commands\Internal\Support\GitHubReleaseData;
 use App\Commands\Internal\Support\GitHubReleaseAsset;
 
 it('creates a GitHubRelease object from JSON data', function () {
-    expect(new GitHubReleaseData(fixture('github-release-api-sample-response.json')))
+    expect(new GitHubReleaseData(getFixture()))
         ->toBeInstanceOf(GitHubReleaseData::class);
 });
 
 it('creates a GitHubReleaseAsset object from JSON data', function () {
-    expect(new GitHubReleaseAsset(fixture('github-release-api-sample-response.json')['assets'][0]))
+    expect(new GitHubReleaseAsset(getFixture('assets.0')))
         ->toBeInstanceOf(GitHubReleaseAsset::class);
 });
 
 it('constructs semver tag', function () {
-    $release = new GitHubReleaseData(fixture('github-release-api-sample-response.json'));
+    $release = new GitHubReleaseData(getFixture());
 
     expect($release->tag)->toBe('v1.0.0');
 });
 
 it('constructs assets', function () {
-    $release = new GitHubReleaseData(fixture('github-release-api-sample-response.json'));
+    $release = new GitHubReleaseData(getFixture());
 
     expect($release->assets)
         ->toHaveCount(1)
@@ -37,13 +38,22 @@ test('asset class throws an exception when required fields are missing', functio
 })->throws(InvalidArgumentException::class);
 
 test('data class throws an exception when required field is missing', function () {
-    $data = fixture('github-release-api-sample-response.json');
+    $data = getFixture();
     array_shift($data);
     new GitHubReleaseData($data);
 })->throws(InvalidArgumentException::class);
 
 test('asset class throws an exception when required field is missing', function () {
-    $data = fixture('github-release-api-sample-response.json')['assets'][0];
+    $data = getFixture('assets.0');
     array_shift($data);
     new GitHubReleaseAsset($data);
 })->throws(InvalidArgumentException::class);
+
+function getFixture(?string $selector = null): array
+{
+    if ($selector !== null) {
+        return Arr::get(getFixture(), $selector);
+    }
+
+    return fixture('github-release-api-sample-response.json');
+}
