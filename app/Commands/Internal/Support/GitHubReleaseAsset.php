c9bae530a5d75dd76ed629a0f0689023e4775a71
Caen De Silva <caen@desilva.se>
2024-04-24 11:37:26 +0200

    Refactor validation handling

--- app/Commands/Internal/Support/GitHubReleaseAsset.php
+++ app/Commands/Internal/Support/GitHubReleaseAsset.php
@@ -4,7 +4,12 @@ declare(strict_types=1);
 
 namespace App\Commands\Internal\Support;
 
+use Illuminate\Support\Arr;
 use InvalidArgumentException;
+
+use function implode;
+use function array_keys;
+use function array_diff;
 
 /**
  * @internal Helper class to provide a typed representation of a GitHub release asset in the GitHubReleaseData class.
@@ -20,17 +25,10 @@ class GitHubReleaseAsset
 
     protected function validate(array $data): void
     {
-        // Perform validation based on the GitHub API response structure schema.
+        $requiredFields = ['url', 'id', 'node_id', 'name', 'label', 'uploader', 'content_type', 'state', 'size', 'download_count', 'created_at', 'updated_at', 'browser_download_url'];
 
-        $requiredFields = [
-            'url', 'id', 'node_id', 'name', 'label', 'uploader', 'content_type',
-            'state', 'size', 'download_count', 'created_at', 'updated_at', 'browser_download_url'
-        ];
-
-        foreach ($requiredFields as $field) {
-            if (! isset($data[$field])) {
-                throw new InvalidArgumentException("Missing required field: $field");
-            }
+        if (! Arr::has($data, $requiredFields)) {
+            throw new InvalidArgumentException('Missing required field(s): '.implode(', ', array_diff($requiredFields, array_keys($data))));
         }
     }
 }
--- app/Commands/Internal/Support/GitHubReleaseData.php
+++ app/Commands/Internal/Support/GitHubReleaseData.php
@@ -4,7 +4,12 @@ declare(strict_types=1);
 
 namespace App\Commands\Internal\Support;
 
+use Illuminate\Support\Arr;
 use InvalidArgumentException;
+
+use function implode;
+use function array_keys;
+use function array_diff;
 
 /**
  * @internal Helper class for the self-update command to wrap the GitHub release data API response.
@@ -20,18 +25,10 @@ class GitHubReleaseData
 
     protected function validate(array $data): void
     {
-        // Perform validation based on the GitHub API response structure schema.
+        $requiredFields = ['url', 'html_url', 'assets_url', 'upload_url', 'tarball_url', 'zipball_url', 'id', 'node_id', 'tag_name', 'target_commitish', 'draft', 'prerelease', 'created_at', 'author', 'assets'];
 
-        $requiredFields = [
-            'url', 'html_url', 'assets_url', 'upload_url', 'tarball_url',
-            'zipball_url', 'id', 'node_id', 'tag_name', 'target_commitish',
-            'draft', 'prerelease', 'created_at', 'author', 'assets'
-        ];
-
-        foreach ($requiredFields as $field) {
-            if (! isset($data[$field])) {
-                throw new InvalidArgumentException("Missing required field: $field");
-            }
+        if (! Arr::has($data, $requiredFields)) {
+            throw new InvalidArgumentException('Missing required field(s): '.implode(', ', array_diff($requiredFields, array_keys($data))));
         }
     }
 }
