b4e7a82548d445223e96792023a31c2ac1380348
Caen De Silva <caen@desilva.se>
2024-04-24 12:16:03 +0200

    Add test data in setup function

--- tests/Unit/Support/GitHubReleaseDataTest.php
+++ tests/Unit/Support/GitHubReleaseDataTest.php
@@ -3,24 +3,28 @@
 use App\Commands\Internal\Support\GitHubReleaseData;
 use App\Commands\Internal\Support\GitHubReleaseAsset;
 
+beforeEach(function () {
+    $this->data = fixture('github-release-api-sample-response.json');
+});
+
 it('creates a GitHubRelease object from JSON data', function () {
-    expect(new GitHubReleaseData(fixture('github-release-api-sample-response.json')))
+    expect(new GitHubReleaseData($this->data))
         ->toBeInstanceOf(GitHubReleaseData::class);
 });
 
 it('creates a GitHubReleaseAsset object from JSON data', function () {
-    expect(new GitHubReleaseAsset(fixture('github-release-api-sample-response.json')['assets'][0]))
+    expect(new GitHubReleaseAsset($this->data['assets'][0]))
         ->toBeInstanceOf(GitHubReleaseAsset::class);
 });
 
 it('constructs semver tag', function () {
-    $release = new GitHubReleaseData(fixture('github-release-api-sample-response.json'));
+    $release = new GitHubReleaseData($this->data);
 
     expect($release->tag)->toBe('v1.0.0');
 });
 
 it('constructs assets', function () {
-    $release = new GitHubReleaseData(fixture('github-release-api-sample-response.json'));
+    $release = new GitHubReleaseData($this->data);
 
     expect($release->assets)
         ->toHaveCount(1)
@@ -37,13 +41,13 @@ test('asset class throws an exception when required fields are missing', functio
 })->throws(InvalidArgumentException::class);
 
 test('data class throws an exception when required field is missing', function () {
-    $data = fixture('github-release-api-sample-response.json');
+    $data = $this->data;
     array_shift($data);
     new GitHubReleaseData($data);
 })->throws(InvalidArgumentException::class);
 
 test('asset class throws an exception when required field is missing', function () {
-    $data = fixture('github-release-api-sample-response.json')['assets'][0];
+    $data = $this->data['assets'][0];
     array_shift($data);
     new GitHubReleaseAsset($data);
 })->throws(InvalidArgumentException::class);
