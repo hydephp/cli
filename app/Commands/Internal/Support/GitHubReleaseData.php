cbbc980ac115f538c4e591bc8c9537bf5f79c6df
Caen De Silva <caen@desilva.se>
2024-04-24 10:13:24 +0200

    Link to API docs

--- app/Commands/Internal/Support/GitHubReleaseData.php
+++ app/Commands/Internal/Support/GitHubReleaseData.php
@@ -6,6 +6,8 @@ namespace App\Commands\Internal\Support;
 
 /**
  * @internal Helper class for the self-update command to wrap the GitHub release data API response.
+ *
+ * @link https://docs.github.com/en/rest/releases/releases?apiVersion=2022-11-28#get-the-latest-release
  */
 class GitHubReleaseData
 {
