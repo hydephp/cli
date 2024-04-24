e6987723dea148f964e384a126989f587eb6e4ed
Caen De Silva <caen@desilva.se>
2024-04-24 10:19:01 +0200

    Create GitHubReleaseAsset.php

---
+++ app/Commands/Internal/Support/GitHubReleaseAsset.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Commands\Internal\Support;
+
+/**
+ * @internal Helper class to provide a typed representation of a GitHub release asset in the GitHubReleaseData class.
+ *
+ * @see \App\Commands\Internal\Support\GitHubReleaseData
+ */
+class GitHubReleaseAsset
+{
+    //
+}
