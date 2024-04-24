b9219da2f99bf1ac6020176e8a3ad5f8f448f75f
Caen De Silva <caen@desilva.se>
2024-04-24 10:11:19 +0200

    Create GitHubReleaseData.php

---
+++ app/Commands/Internal/Support/GitHubReleaseData.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Commands\Internal\Support;
+
+/**
+ * @internal Helper class for the self-update command to wrap the GitHub release data API response.
+ */
+class GitHubReleaseData
+{
+    //
+}
