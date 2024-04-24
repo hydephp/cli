a8806b0f4d7508d2ca15c2b9cb866b306c9df667
Caen De Silva <caen@desilva.se>
2024-04-24 15:51:00 +0200

    Create ConfigFileTest.php

---
+++ tests/Unit/ConfigFileTest.php
@@ -0,0 +1,7 @@
+<?php
+
+test('config file has the correct structure', function () {
+    $config = require __DIR__.'/../../app/config.php';
+
+    expect($config)->toHaveKeys(['name', 'version', 'env', 'providers', 'aliases',]);
+});
