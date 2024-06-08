1ea85434c358575ab2851736681c91078a62a66e
Caen De Silva <caen@desilva.se>
2024-06-08 20:19:35 +0200

    Add dd helper

--- bin/build-manual.php
+++ bin/build-manual.php
@@ -7,3 +7,10 @@ function hyde(string $command): string
 {
     return shell_exec("php hyde $command");
 }
+
+/** Dump and die with the given data. */
+function dd(mixed $data): void
+{
+    var_dump($data);
+    die;
+}
