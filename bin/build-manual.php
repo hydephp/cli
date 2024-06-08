0778f4c9a0c22f53f6c18b113012a6fd3f1df30b
Caen De Silva <caen@desilva.se>
2024-06-08 20:19:10 +0200

    Add exec helper

--- bin/build-manual.php
+++ bin/build-manual.php
@@ -1,3 +1,9 @@
 <?php
 
 /** @internal Build the documentation manual. */
+
+/** Execute a command in the Hyde CLI and return the output. */
+function hyde(string $command): string
+{
+    return shell_exec("php hyde $command");
+}
