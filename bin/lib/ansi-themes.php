6f292f96c7c5aa6fd923207e17453c37e215d50f
Caen De Silva <caen@desilva.se>
2024-06-08 21:49:39 +0200

    Create class ClassicTheme

--- bin/lib/ansi-themes.php
+++ bin/lib/ansi-themes.php
@@ -16,3 +16,30 @@ interface ThemeInterface {
      */
     public static function background(): string;
 }
+
+class ClassicTheme implements ThemeInterface
+{
+    public static function colors(): array
+    {
+        return [
+            30 => '#000', // Black
+            31 => '#f00', // Red
+            32 => '#0f0', // Green
+            33 => '#ff0', // Yellow
+            34 => '#00f', // Blue
+            35 => '#f0f', // Magenta
+            36 => '#0ff', // Cyan
+            37 => '#fff', // White
+        ];
+    }
+
+    public static function fontFamily(): string
+    {
+        return 'monospace';
+    }
+
+    public static function background(): string
+    {
+        return '#000';
+    }
+}
