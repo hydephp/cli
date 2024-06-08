4f55d6537854bd4b145304b802aadb41c329f576
Caen De Silva <caen@desilva.se>
2024-06-08 20:20:08 +0200

    Require autoloader

--- bin/build-manual.php
+++ bin/build-manual.php
@@ -2,6 +2,8 @@
 
 /** @internal Build the documentation manual. */
 
+require_once __DIR__ . '/../vendor/autoload.php';
+
 /** Execute a command in the Hyde CLI and return the output. */
 function hyde(string $command): string
 {
