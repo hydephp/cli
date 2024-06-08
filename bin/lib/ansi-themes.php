e19093e89019bc4c03c3ef5041ed1c4e48c41876
Caen De Silva <caen@desilva.se>
2024-06-08 21:49:04 +0200

    Implement rest of the contract

--- bin/lib/ansi-themes.php
+++ bin/lib/ansi-themes.php
@@ -2,7 +2,17 @@
 
 interface ThemeInterface {
     /**
-     * @return array<int, string>
+     * @return array<int, string> The color codes to use for the terminal. (Ansi 30-37)
      */
     public static function colors(): array;
+
+    /**
+     * @return string The font family to use for the terminal. (CSS font-family)
+     */
+    public static function fontFamily(): string;
+
+    /**
+     * @return string The background color to use for the terminal. (HTML color code
+     */
+    public static function background(): string;
 }
