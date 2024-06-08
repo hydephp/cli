3d9af41aee8f440476b78ce12441b1a49dd271c2
Caen De Silva <caen@desilva.se>
2024-06-08 20:22:11 +0200

    Change into the root directory

--- bin/build-manual.php
+++ bin/build-manual.php
@@ -4,8 +4,10 @@
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
+chdir(__DIR__ . '/..');
+
 /** Execute a command in the Hyde CLI and return the output. */
 function hyde_exec(string $command): string
 {
-    return shell_exec("php ../hyde $command");
+    return shell_exec("php hyde $command");
 }
