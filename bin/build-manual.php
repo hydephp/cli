e508fed523534abe6164a67b4015142e856e26d9
Caen De Silva <caen@desilva.se>
2024-06-08 22:17:51 +0200

    Pre wrap whitespace

--- bin/build-manual.php
+++ bin/build-manual.php
@@ -128,6 +128,7 @@ function ansi_html_theme(): string
              font-family: {$theme::fontFamily()};
              font-size: 12px;
              width: 120ch;
+             white-space: pre-wrap;
              padding: 1em;
         }
 CSS;
