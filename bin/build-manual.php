2266a58f927cf031611895bf41f07c7167c6ec2e
Caen De Silva <caen@desilva.se>
2024-06-08 20:20:52 +0200

    Fix path

--- bin/build-manual.php
+++ bin/build-manual.php
@@ -7,5 +7,5 @@ require_once __DIR__ . '/../vendor/autoload.php';
 /** Execute a command in the Hyde CLI and return the output. */
 function hyde(string $command): string
 {
-    return shell_exec("php hyde $command");
+    return shell_exec("php ../hyde $command");
 }
