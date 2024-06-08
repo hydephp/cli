dbb8b1be6935b707f23c656913281c74ddd5e81a
Caen De Silva <caen@desilva.se>
2024-06-08 20:36:51 +0200

    Support returning output

--- bin/build-manual.php
+++ bin/build-manual.php
@@ -16,9 +16,11 @@ if (! is_dir('docs/manual')) {
     mkdir('docs/manual', recursive: true);
 }
 
-task('Getting command list', 'Got command list', function (): void {
+$commands = task('Getting command list', 'Got command list', function (): array {
     $commands = hyde_exec('list --format=json --no-ansi');
     $commands = json_decode($commands, true);
+
+    return $commands;
 });
 
 task('Building XML manual', 'Built XML manual', function (): void {
@@ -31,12 +33,14 @@ task('Building Markdown manual', 'Built Markdown manual', function (): void {
     file_put_contents('docs/manual/manual.md', $md);
 });
 
-function task(string $start, string $end, callable $task): void {
+function task(string $start, string $end, callable $task): mixed {
     $timeStart = microtime(true);
     echo "$start...";
 
-    $task();
+    $output = $task();
 
     $time = round((microtime(true) - $timeStart) * 1000, 2);
     echo "\r$end ($time ms)\n";
+
+    return $output;
 }
