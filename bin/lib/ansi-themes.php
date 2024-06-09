e8dfc1e2cfb2166a02ce0385a66570523d54022e
Caen De Silva <caen@desilva.se>
2024-06-09 12:02:21 +0200

    Revert "Refactor to use better OOP"
    This reverts commit 16acdc2382b832c392d2bbf9439e2c6fa4a73226.

--- bin/lib/ansi-themes.php
+++ bin/lib/ansi-themes.php
@@ -4,122 +4,116 @@ interface ThemeInterface {
     /**
      * @return array<int, string> The color codes to use for the terminal. (Ansi 30-37)
      */
-    public function getColors(): array;
+    public static function colors(): array;
 
     /**
      * @return string The background color to use for the terminal. (HTML color code)
      */
-    public function getBackground(): string;
+    public static function background(): string;
 
     /**
      * @return string The default text color to use for the terminal. (HTML color code)
      */
-    public function getTextColor(): string;
+    public static function textColor(): string;
 
     /**
      * @return string The font family to use for the terminal. (CSS font-family)
      */
-    public function getFontFamily(): string;
+    public static function fontFamily(): string;
 }
 
-abstract class AbstractTheme implements ThemeInterface
+class ClassicTheme implements ThemeInterface
 {
-    protected array $colors;
-    protected string $background;
-    protected string $textColor;
-    protected string $fontFamily;
-
-    public function __construct(array $colors, string $background, string $textColor, string $fontFamily)
+    public static function colors(): array
     {
-        $this->colors = $colors;
-        $this->background = $background;
-        $this->textColor = $textColor;
-        $this->fontFamily = $fontFamily;
+        return [
+            30 => '#000', // Black
+            31 => '#f00', // Red
+            32 => '#0f0', // Green
+            33 => '#ff0', // Yellow
+            34 => '#00f', // Blue
+            35 => '#f0f', // Magenta
+            36 => '#0ff', // Cyan
+            37 => '#fff', // White
+        ];
     }
 
-    public function getColors(): array
+    public static function background(): string
     {
-        return $this->colors;
+        return '#000';
     }
 
-    public function getBackground(): string
+    public static function textColor(): string
     {
-        return $this->background;
+        return '#fff';
     }
 
-    public function getTextColor(): string
+    public static function fontFamily(): string
     {
-        return $this->textColor;
-    }
-
-    public function getFontFamily(): string
-    {
-        return $this->fontFamily;
+        return 'monospace';
     }
 }
 
-class ClassicTheme extends AbstractTheme
+class FiraTheme implements ThemeInterface
 {
-    public function __construct()
+    public static function colors(): array
     {
-        parent::__construct(
-            [
-                30 => '#000', // Black
-                31 => '#f00', // Red
-                32 => '#0f0', // Green
-                33 => '#ff0', // Yellow
-                34 => '#00f', // Blue
-                35 => '#f0f', // Magenta
-                36 => '#0ff', // Cyan
-                37 => '#fff', // White
-            ],
-            '#000', // Background
-            '#fff', // Text Color
-            'monospace' // Font Family
-        );
+        return [
+            30 => '#000', // Black
+            31 => '#f44336', // Red
+            32 => '#4caf50', // Green
+            33 => '#ffeb3b', // Yellow
+            34 => '#2196f3', // Blue
+            35 => '#9c27b0', // Magenta
+            36 => '#00bcd4', // Cyan
+            37 => '#fff', // White
+        ];
+    }
+
+    public static function background(): string
+    {
+        return '#263238';
+    }
+
+    public static function textColor(): string
+    {
+        return '#fff';
+    }
+
+    public static function fontFamily(): string
+    {
+        return 'Fira Code, monospace';
     }
 }
 
-class FiraTheme extends AbstractTheme
+class CampbellTheme implements ThemeInterface
 {
-    public function __construct()
+    public static function colors(): array
     {
-        parent::__construct(
-            [
-                30 => '#000', // Black
-                31 => '#f44336', // Red
-                32 => '#4caf50', // Green
-                33 => '#ffeb3b', // Yellow
-                34 => '#2196f3', // Blue
-                35 => '#9c27b0', // Magenta
-                36 => '#00bcd4', // Cyan
-                37 => '#fff', // White
-            ],
-            '#263238', // Background
-            '#fff', // Text Color
-            'Fira Code, monospace' // Font Family
-        );
+        return [
+            30 => '#0C0C0C', // Black
+            31 => '#C50F1F', // Red
+            32 => '#13A10E', // Green
+            33 => '#C19C00', // Yellow
+            34 => '#0037DA', // Blue
+            35 => '#881798', // Magenta
+            36 => '#3A96DD', // Cyan
+            37 => '#CCCCCC', // White
+        ];
     }
-}
 
-class CampbellTheme extends AbstractTheme
-{
-    public function __construct()
+    public static function background(): string
     {
-        parent::__construct(
-            [
-                30 => '#0C0C0C', // Black
-                31 => '#C50F1F', // Red
-                32 => '#13A10E', // Green
-                33 => '#C19C00', // Yellow
-                34 => '#0037DA', // Blue
-                35 => '#881798', // Magenta
-                36 => '#3A96DD', // Cyan
-                37 => '#CCCCCC', // White
-            ],
-            '#0C0C0C', // Background
-            '#CCCCCC', // Text Color
-            'monospace' // Font Family
-        );
+        return '#0C0C0C';
+    }
+
+    public static function textColor(): string
+    {
+        return '#CCCCCC';
+    }
+
+    public static function fontFamily(): string
+    {
+        return 'monospace';
     }
 }
