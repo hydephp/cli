8311925d0d2b12b62d95cf89427e748d6e93888b
Caen De Silva <caen@desilva.se>
2024-06-09 17:34:10 +0200

    Wrap signature in classed div

--- bin/build-manual.php
+++ bin/build-manual.php
@@ -34,7 +34,7 @@ task('building|built', 'Html manual', function () use ($commands): void {
         <section>
         <h2>$name</h2>
 
-        <pre class="terminal-screen">$signature
+        <pre class="terminal-screen"><div class="signature">$signature</div>
         $info</pre>
 
         </section>
