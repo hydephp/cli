efa3201ef83b3e6db8cab5085a2c9feac180d874
caendesilva <95144705+caendesilva@users.noreply.github.com>
2024-06-11 12:55:10 +0000

    HydeCLI v0.9.4

--- app/Application.php
+++ app/Application.php
@@ -6,7 +6,7 @@ namespace App;
 
 class Application extends \Hyde\Foundation\Application
 {
-    final public const APP_VERSION = '0.9.3';
+    final public const APP_VERSION = '0.9.4';
 
     public function getCachedPackagesPath(): string
     {
